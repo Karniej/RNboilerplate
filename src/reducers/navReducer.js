710654851bdbd17066864bef52753d2014628b7e
Paweł Karniej <karniej.p@gmail.com>
2018-01-08 22:30:25 +0100

    deleted some spare code

--- src/reducers/navReducer.js
+++ src/reducers/navReducer.js
@@ -1,7 +1,6 @@
 // @flow
 import navigator from '../navigation/navigator';
 
-// const initialState = navigator.router.getStateForAction(navigator.router.getActionForPathAndParams('loginStack'));
 const navReducer = (state: any, action: any) => {
   const newState = navigator.router.getStateForAction(action, state);
   return newState || state;
