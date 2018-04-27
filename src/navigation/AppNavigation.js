5d5d940e478ddcbf8e673ac2c52639cbd795ad8a
Karniej <karniej.p@gmail.com>
2018-04-27 10:24:01 +0200

    Fixed eslint problems with jsx

--- src/navigation/AppNavigation.js
+++ src/navigation/AppNavigation.js
@@ -4,16 +4,16 @@ import * as ReactNavigation from 'react-navigation';
 import PropTypes from 'prop-types';
 import Navigator from './navigator';
 
-const AppNavigation = (navigationState, dispatch) => {
+const AppNavigation = ({ navigationState, dispatch }) => {
   const navigation = ReactNavigation.addNavigationHelpers({
     dispatch,
     state: navigationState,
   });
   return <Navigator navigation={navigation} />;
 };
-AppNavigation.PropTypes = {
-  navigationState: PropTypes.object,
-  dispatch: PropTypes.func,
+AppNavigation.propTypes = {
+  navigationState: PropTypes.func.isRequired,
+  dispatch: PropTypes.func.isRequired,
 };
 const mapStateToProps = state => ({
   navigationState: state.nav,
