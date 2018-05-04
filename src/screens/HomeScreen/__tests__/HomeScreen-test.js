9086cb2846f190afc4d6c52d665ff8f99870f4e2
Karniej <karniej.p@gmail.com>
2018-05-04 16:04:48 +0200

    Added enzyme for unit testing

--- src/screens/HomeScreen/__tests__/HomeScreen-test.js
+++ src/screens/HomeScreen/__tests__/HomeScreen-test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { shallow, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import HomeScreen from '../HomeScreen';
+
+configure({ adapter: new Adapter() });
 
 test('Renders correctly, expect onpress to be defined, but not called', () => {
   const navigation = { navigate: jest.fn() };
   const onPress = jest.fn();
+  const wrapper = shallow(<HomeScreen navigation={navigation} onPress={onPress} />);
   expect(onPress).toBeDefined();
   expect(onPress).not.toBeCalled();
-  expect(renderer.create(<HomeScreen navigation={navigation} onPress={onPress} />).toJSON()).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
