582b0f1cc7dc837292d70a1fa77ddcae666a34f7
Paweł Karniej <pawel.karniej@netguru.pl>
2018-05-27 12:47:29 +0200

    added the demo components

--- src/screens/HomeScreen/styles.js
+++ src/screens/HomeScreen/styles.js
@@ -1,7 +1,7 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  mainContainer: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
@@ -11,6 +11,20 @@ const styles = StyleSheet.create({
     fontSize: 20,
     textAlign: 'center',
     margin: 10,
+  },
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    display: 'none',
+  },
+  subContainer: {
+    flex: 2,
+    justifyContent: 'center',
+  },
+  textStyle: {
+    fontSize: 25,
+    textAlign: 'center',
+    paddingHorizontal: 10,
   },
 });
 export default styles;
