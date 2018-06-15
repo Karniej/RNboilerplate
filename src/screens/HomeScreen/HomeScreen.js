208be9cea4083a566053aaf66c72e888568861bd
Paweł Karniej <pawel.karniej@netguru.pl>
2018-06-15 14:57:17 +0200

    deleted spare styles file

--- src/screens/HomeScreen/HomeScreen.js
+++ src/screens/HomeScreen/HomeScreen.js
@@ -1,13 +1,9 @@
 import * as React from 'react'
-import { Text, View } from 'react-native'
 import PropTypes from 'prop-types'
-import styles from './styles'
 import Header from '../../components/Header/Header'
 import MainButton from '../../components/MainButton/MainButton'
 import ContentWrapper from '../../components/ContentWrapper/ContentWrapper'
-import { colors } from '../../config'
 
-const { container, textStyle, subContainer } = styles
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
@@ -30,27 +26,6 @@ class HomeScreen extends React.Component {
           routeName='Second'
           navigation={navigation}
         />
-        <View
-          style={container}
-        >
-          <View
-            style={[subContainer, { backgroundColor: colors.accentColor }]}
-          >
-            <Text
-              style={[textStyle, { color: colors.secondaryColor }]}
-            >
-              Mobile Discussion Panel
-            </Text>
-          </View>
-          <View
-            style={[subContainer, { backgroundColor: colors.secondaryColor }]}
-          >
-            <Text
-              style={[textStyle, { color: colors.primaryColor }]}
-            >Ok , it's enough
-            </Text>
-          </View>
-        </View>
       </ContentWrapper>
     )
   }
